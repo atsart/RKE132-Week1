d61df697614607c0e733f85fd17ccaeef2bbb8be
Ats Art <ats.art.ilves@tktk.ee>
2024-04-23 23:32:31 +0300

    add week 1 code

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// This is a comment to my code
+
+//Rakendus küsi kasutaja nime
+//Rakendus tervitab kasutajat nimepidi
+
+using System.Net.Security;
+
+Console.WriteLine("Enter your name:"); //Output
+//String=sõne
+string userName = Console.ReadLine();//Output
+
+Console.WriteLine("Hello " + userName +"!"); //Output
+
+//string interpolation
+
+Console.WriteLine($"Hello, { userName} !"); //Output
